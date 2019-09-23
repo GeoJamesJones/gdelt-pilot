adcbb10ce9b8dc4263b205b6143877008ed862e7
James Jones <james_jones@esri.com>
2019-09-23 11:13:50 -0400

    added validation

--- get_missing_data.py
+++ get_missing_data.py
@@ -5,6 +5,8 @@ import zipfile
 outdir = "test_data"
 csv_dir = "csv"
 
+
+
 count = 0
 with open('data/masterfilelist.txt', 'r') as txt:
     contents = txt.read().split("\n")
@@ -16,22 +18,23 @@ with open('data/masterfilelist.txt', 'r') as txt:
             int_zip_file = os.path.join(outdir, outname)
             if "export" in filename:
                 if outname.startswith("201504") or outname.startswith("201505") or outname.startswith("201506") or outname.startswith("201507") or outname.startswith("201508") or outname.startswith("201509") or outname.startswith("201510") or outname.startswith("201511") or outname.startswith("201512"):
-                print(filename)
-                headers = {
-                    'Accept': "*/*",
-                    'Cache-Control': "no-cache",
-                    'Host': "data.gdeltproject.org",
-                    'Accept-Encoding': "gzip, deflate",
-                    'Connection': "keep-alive",
-                    'cache-control': "no-cache"
-                    }
+                if outname.splitext()[0] + ".geojson" not in completed_files:
+                    print(filename)
+                    headers = {
+                        'Accept': "*/*",
+                        'Cache-Control': "no-cache",
+                        'Host': "data.gdeltproject.org",
+                        'Accept-Encoding': "gzip, deflate",
+                        'Connection': "keep-alive",
+                        'cache-control': "no-cache"
+                        }
 
-                response = requests.request("GET", filename, headers=headers)
-                open(int_zip_file, 'wb').write(response.content)
-                print(response.status_code)
+                    response = requests.request("GET", filename, headers=headers)
+                    open(int_zip_file, 'wb').write(response.content)
+                    print(response.status_code)
 
-                with zipfile.ZipFile(int_zip_file, 'r') as zip_ref:
-                    zip_ref.extractall(csv_dir)
+                    with zipfile.ZipFile(int_zip_file, 'r') as zip_ref:
+                        zip_ref.extractall(csv_dir)
 
         except Exception as e:
             print(e)
